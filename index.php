c7a6b96a2239fa8e8b5a1b7a9ec724de805895c5
Andrew Church <andrewchurch@gmail.com>
2010-09-12 11:07:44 -0400

    Added Twitter functionality.

--- index.php
+++ index.php
@@ -34,88 +34,37 @@
 				<p>In 2005, I was named to the Baltimore Business Journal's list of &quot;Top 40 Under 40&quot;. I hold an M.S. from the University of Maryland and a B.S. from Roanoke College. I live in Baltimore with my wife Nicole and our sons, Carter and Bennett.</p> 
 			</div> 
 			
-			<div id="twitter"> 
-				<h3><span>Twitter</span></h3> 
-				<p>Font Shop's Field Guide to Love and Typography is ABSOLUTELY worth the free download. Get it now. <a href="http://is.gd/diHiP" target="_blank">http://is.gd/diHiP</a> <span>@<a href="http://www.twitter.com/markjmaloney" target="_blank">markjmaloney</a> about 15 min ago</span></p> 
-				<p>Congrats! RT @<a href="http://www.twitter.com/gregcangialosi" target="_blank">gregcangialosi</a>: @<a href="http://www.twitter.com/gregcangialosi" target="_blank">gregcangialosi</a> Blue Sky Factory was just named one of DC Area's Hottest Companies - <a href="http://is.gd/dhCde" target="_blank">http://is.gd/dhCde</a> <span>@<a href="http://www.twitter.com/markjmaloney" target="_blank">markjmaloney</a> about 2 hrs ago</span></p> 
-				<p>@<a href="http://www.twitter.com/paulkittredge" target="_blank">paulkittredge</a> Less than 5% of my traffic has resolution below 1280...but I think that you're right. It just need to be better not bigger. <span>@<a href="http://www.twitter.com/markjmaloney" target="_blank">markjmaloney</a> about 3 hrs ago</span></p> 
-			</div> 
-			
-			<div id="facebook"> 
-				<h3><span>Facebook</span></h3> 
-				<p>Mid-century corporate design. Tight. <a href="http://is.gd/diHiP" target="_blank"><img src="assets/images/fpo_facebook_thumb.jpg" width="45" height="36" alt="Facebook Thumb">Mid-Century Corporate Design – Part 1 | Aqua-Velvet</a> <span>about 8 hrs ago</span></p> 
-				<ul> 
-				<li><img src="assets/images/fpo_avatar.jpg" width="24" height="24" alt="Facebook Avatar">Fabulous</li> 
-				<li><img src="assets/images/fpo_avatar.jpg" width="24" height="24" alt="Facebook Avatar">Great find.  The IBM stuff is solid.</li> 
-				<li><img src="assets/images/fpo_avatar.jpg" width="24" height="24" alt="Facebook Avatar">Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li> 
-				</ul> 
-			</div> 
-			
-			<div id="lastfm"> 
-				<h3><span>Last.FM</span></h3> 
-				<p><a href="http://www.last.fm" target="_blank"><img src="assets/images/fpo_lastfm_thumb.jpg" width="34" height="34" alt="Last.FM Thumb">Bonobo - Kong</a> <span>about 15 min ago</span></p> 
-				<p><a href="http://www.last.fm" target="_blank"><img src="assets/images/fpo_lastfm_thumb.jpg" width="34" height="34" alt="Last.FM Thumb">Altern 8 – Altern8 @The Clarence Sligo 18-4-2009</a> <span>Yesterday 2:10pm</span></p> 
-				<p><a href="http://www.last.fm" target="_blank"><img src="assets/images/fpo_lastfm_thumb.jpg" width="34" height="34" alt="Last.FM Thumb">Murk – Essential Mix - 27-MAR-2005 - Pt.3 - Murk</a> <span>Yesterday 12:18pm</span></p> 
-			</div> 
-			
+			<div id="socialMedia">
+
+				<div class="block">
+
+					<div id="facebook"> 
+						<h3><span>Facebook</span></h3> 
+						<p>Mid-century corporate design. Tight. <a href="http://is.gd/diHiP" target="_blank"><img src="assets/images/fpo_facebook_thumb.jpg" width="45" height="36" alt="Facebook Thumb">Mid-Century Corporate Design – Part 1 | Aqua-Velvet</a> <span>about 8 hrs ago</span></p> 
+						<ul> 
+						<li><img src="assets/images/fpo_avatar.jpg" width="24" height="24" alt="Facebook Avatar">Fabulous</li> 
+						<li><img src="assets/images/fpo_avatar.jpg" width="24" height="24" alt="Facebook Avatar">Great find.  The IBM stuff is solid.</li> 
+						<li><img src="assets/images/fpo_avatar.jpg" width="24" height="24" alt="Facebook Avatar">Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li> 
+						</ul> 
+					</div> 
+					
+					<div id="lastfm"> 
+						<h3><span>Last.FM</span></h3> 
+						<p><a href="http://www.last.fm" target="_blank"><img src="assets/images/fpo_lastfm_thumb.jpg" width="34" height="34" alt="Last.FM Thumb">Bonobo - Kong</a> <span>about 15 min ago</span></p> 
+						<p><a href="http://www.last.fm" target="_blank"><img src="assets/images/fpo_lastfm_thumb.jpg" width="34" height="34" alt="Last.FM Thumb">Altern 8 – Altern8 @The Clarence Sligo 18-4-2009</a> <span>Yesterday 2:10pm</span></p> 
+						<p><a href="http://www.last.fm" target="_blank"><img src="assets/images/fpo_lastfm_thumb.jpg" width="34" height="34" alt="Last.FM Thumb">Murk – Essential Mix - 27-MAR-2005 - Pt.3 - Murk</a> <span>Yesterday 12:18pm</span></p> 
+					</div> 
+				
+				</div>
+				
+			</div>
+		
 		</div> 
 	 
 	</div>
 
 	<?php include $_SERVER['DOCUMENT_ROOT'] . '/lib/includes/js.inc.php' ?>
-	<script>
-	(function () {
-		
-		$('#screenshots').wrapInner('<div id="pane" />').wrapInner('<div id="paneWrapper" />').find('img').show();
-		var $pane = $('#pane'),
-			fixRevert = false,
-			initPos = 0,
-			pos = 0,
-			numImages = $('#pane img').length,
-			imageWidth = 770,
-			maxPos = parseInt('-' + (numImages - 1) * imageWidth, 10);
-		
-		(function initDraggable() {
-			$pane.draggable({
-				'axis': 'x',
-				'revert': true,
-				'drag': function () {
-					
-					// current pane position
-					pos = parseInt(this.style.left.replace('px',''), 10);
-				
-					// if we are inside the bounds of the images, and onto a new slide
-					// then clear the revert, spoof it, and reinit the draggable :)
-					if (pos < 0 && pos >= maxPos && !fixRevert) {
-						if(Math.abs(initPos - pos) > imageWidth / 2) {
-							$pane.draggable('option', 'revert', false);
-							fixRevert = true;
-						}
-					}
-				},
-				'stop': function () {
-					if (fixRevert) {
-						
-						// remove draggable so we can redo it with a new starting point
-						$pane.draggable('destroy');
-						
-						// spoof revert to nearest slide
-						initPos = Math.round(pos / imageWidth) * imageWidth;
-						$pane.animate({
-							'left': initPos + 'px'
-						}, 500, null, function () {
-							
-							// then reinitialize draggable (in new position)
-							fixRevert = false;
-							initDraggable();
-						});
-					}
-				}
-			});
-		})();
-		
-	})();
-	</script>
+	<script src="/lib/js/mjm-home.js"></script>
 
 </body>
 </html>
